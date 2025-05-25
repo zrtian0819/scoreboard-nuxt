68431856bbaa13d04b81a0fa7fda9ca6aee6e047
Joe_Zhang <cjt3399@gmail.com>
2025-05-25 17:08:56 +0800

    feat: 移除buildAssetsDir設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   ssr: false, // 可開可關，GitHub Pages 建議使用 ssr: false
   app: {
     baseURL: '/scoreboard-nuxt/', // 替換為你的倉庫名稱
-    buildAssetsDir: '/_assets/',
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
